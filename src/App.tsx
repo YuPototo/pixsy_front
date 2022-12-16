b8cf9b0df599c8aef858797d5dcb8140ab2c22af
Yu Qin <sirius.qinyu@gmail.com>
2022-12-16 17:08:14 +0800

    Use custome color

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import React from "react";
 import "./App.css";
 import { useAppSelector } from "./app/hooks";
 
 function App() {
     const value = useAppSelector((state) => state.photoList.value);
-    return <div className="bg-red-100 h-32">{value}</div>;
+    return (
+        <div className="bg-primary h-32">
+            {value}
+            <div className="bg-primary-light">abc</div>
+        </div>
+    );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
+        colors: {
+            primary: {
+                DEFAULT: "#018AAB",
+                light: "#C5E0E8",
+            },
+        },
         extend: {},
     },
     plugins: [],
