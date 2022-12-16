771a8e70ffe22e349a14d08614107542fdb14fc7
Yu Qin <sirius.qinyu@gmail.com>
2022-12-16 22:37:28 +0800

    fix skeleton

--- src/components/ImageSkeleton.tsx
+++ src/components/ImageSkeleton.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function ImageSkeleton() {
-    return <div className="w-64 h-48 skeleton"></div>;
+    return <div className="w-64 h-48 skeleton rounded"></div>;
 }
